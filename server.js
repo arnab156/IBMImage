9ecefd325dd196cc9b5f01bf814405d9cf0ccb09
Arnab Majumdar <arnab@Arnabs-MacBook-Pro.local>
2019-01-24 19:19:25 -0600

    codes

--- server.js
+++ server.js
@@ -6,29 +6,32 @@ var visualRecognition = new VisualRecognitionV3({
   iam_apikey: '-n5_SJ9Z5AQXyX4VRN7ktUFVVSSdW7xEg4MFgZVTuNqZ'
 });
 
+
+//ImageRecognition
+
 // var url= 'https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/640px-IBM_VGA_90X8941_on_PS55.jpg';
 // var url='https://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/durchschnittsgesichter/w(01-64)_gr.jpg';
 
 
-// var classifier_ids = ["food"];
-// var url = 'https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/fruitbowl.jpg';
+var classifier_ids = ["food"];
+var url = 'https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/fruitbowl.jpg';
 
-// var params = {
-//   url: url,
-// };
+var params = {
+  url: url,
+};
 
-// visualRecognition.classify(params, function(err, response) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(JSON.stringify(response, null, 2))
-//   }
-// });
+visualRecognition.classify(params, function(err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.stringify(response, null, 2))
+  }
+});
 
 //FaceDetection
 
-var url='https://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/durchschnittsgesichter/w(01-64)_gr.jpg';
-//  var url= 'https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/Ginni_Rometty_at_the_Fortune_MPW_Summit_in_2011.jpg'
+// var url='https://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/durchschnittsgesichter/w(01-64)_gr.jpg';
+var url= 'https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/Ginni_Rometty_at_the_Fortune_MPW_Summit_in_2011.jpg'
 var params1 = {
   url: url,
 };
@@ -37,5 +40,5 @@ visualRecognition.detectFaces(params1, function(err, response) {
   if (err)
     console.log(err);
   else
-    console.log(JSON.stringify(response, null, 2))
+    console.log(JSON.stringify(response, null, 2));
 });
